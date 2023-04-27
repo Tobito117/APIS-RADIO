1ba60b2983f46590f0f9da57e2999d8e0caa3717
Yuliana <yuliana.hdeza@gmail.com>
2023-04-27 09:17:10 -0600

    nada

--- db/connection.ts
+++ db/connection.ts
@@ -3,7 +3,7 @@ import { Sequelize } from 'sequelize';
 
 // const db = new Sequelize('radiocomu', 'system', 'Aldebaran619*', {
 const db = new Sequelize('radiocomu', 'system', 'Aldebaran619*', {
-    host: '10.30.1.44',
+    host: 'localhost',
     dialect: 'mysql',
     // logging: false,
 });
